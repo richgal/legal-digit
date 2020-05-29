18d0ffb768db5530f73cbae01cc134b6784b346c
richgal <richard.gal@gmail.com>
2020-05-29 11:02:56 +0200

    adatkezelés / logo szín

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,2 +1,2 @@
 document.getElementById('footer').innerHTML = 
-'<div class="footer-content"><a href="../../">digitális jogvédelem</a><a href="../rolunk/">rólunk</a><a href="../kapcsolat/">kapcsolat</a></div><div class="footer-content-copy"><p>legaldigital.hu 2020</p></div>';
+'<div class="footer-content"><a href="../../">digitális jogvédelem</a><a href="../rolunk/">rólunk</a><a href="../legal_digital_adatkezelesi tajejoztato.pdf/">adatkezelési tájékoztató</a><a href="../kapcsolat/">kapcsolat</a></div><div class="footer-content-copy"><p>legaldigital.hu 2020</p></div>';
